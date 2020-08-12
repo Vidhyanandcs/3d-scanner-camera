9af0a9fc5282d7e88ba3e0918a26a672d49770df
Vidhyanand CS <vithushaji@gmail.com>
2020-08-12 23:46:59 +0530

    Test endpoint added to camera module

--- camera/src/app.js
+++ camera/src/app.js
@@ -5,6 +5,24 @@ const clear = require('./clear')
 
 const app = express()
 
+//Endpoint for testing
+app.get('/test', function (req, res) {
+
+    clear()
+
+    shoot()
+
+    const cam01Images = zip()
+
+    if(cam01Images){
+        res.send('Working')
+    }else{
+        res.send('Not Working')
+    } 
+    
+})
+
+//Endpoint for capturing
 app.get('/capture', function (req, res) {
 
     clear()
