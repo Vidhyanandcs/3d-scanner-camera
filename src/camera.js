51976ab0ae7af549377aaf60e983699f16ad5745
Vidhyanand CS <vithushaji@gmail.com>
2020-09-24 13:53:34 +0530

    Adding 750ms delay restructuring folder

--- src/camera.js
+++ src/camera.js
@@ -11,7 +11,7 @@ const runApp = async () => {
     const stillCamera = new StillCamera()
 
     const stillCameraWP = new StillCamera({
-        delay: 1000 //delay of 1000ms
+        delay: 750 //delay of 750ms
     })
 
     //To get images with out projection
